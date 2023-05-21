c06fd57eb9d91d69d56633ebd885f28977d7d3c6
Meyzan16 <Meyzan1605@gmail.com>
2023-05-21 14:08:24 +0700

    perbaiki style table

--- src/Card.jsx
+++ src/Card.jsx
@@ -46,7 +46,9 @@ const PromptCard = ({dataa}) => {
   return (
 
       <section className="w-full mt-12  mb-12 relative  overflow-x-auto shadow-md sm:rounded-lg">
-          <div className="w-full  bg-primary bg-opacity-50 px-4 py-4">
+
+
+          <div className="w-full bg-opacity-50 p-4">
            
                 <select value={selectedCategory} onChange={handleChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                  focus:ring-primary focus:border-primary block w-full p-2.5">
@@ -63,8 +65,8 @@ const PromptCard = ({dataa}) => {
                
                 </select>
           </div>
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <table className="w-full text-sm text-left text-gray-500">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                   <tr>
                       
                       <th scope="col" className="px-6 py-3">
